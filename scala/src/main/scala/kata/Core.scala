f96859e71915a09c89d77efa50e8cd6c3b23b31a
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-03-04 14:00:55 +0000

    Where we got to after dojo 1.

--- scala/src/main/scala/kata/Core.scala
+++ scala/src/main/scala/kata/Core.scala
@@ -2,8 +2,22 @@ package kata
 
 class Core {
 
-  def increment(input: Int): Int = {
-    input + 1
+  def calculate(input: String): Either[String, Int] = {
+    println(input.head.toInt)
+
+    val pattern = "(.+) (.+) (.+)".r
+    val pattern(leftValue, operation, rightValue) = input
+
+    (leftValue.toIntOption, rightValue.toIntOption) match {
+      case (None, Some(_)) => Left(s"Error: Invalid input in left value. Expected a number but found '$leftValue'")
+      case (Some(_), None) => Left(s"Error: Invalid input in right value. Expected a number but found '$rightValue'")
+      case (None, None) => Left(s"Error: Invalid input in both left and right values. Expected two numbers but found '$leftValue' and '$rightValue'")
+      case (Some(leftValue), Some(rightValue)) =>
+        operation match {
+          case "+" => Right(leftValue + rightValue)
+          case _ => Left("Error: Invalid input. Binary operator not supported")
+        }
+    }
   }
 
 }
--- scala/src/test/scala/kata/CoreSpec.scala
+++ scala/src/test/scala/kata/CoreSpec.scala
@@ -6,15 +6,88 @@ import matchers.must._
 
 class CoreSpec extends AnyWordSpec with Matchers {
 
-  "increment" must {
-    "return an integer that is 1 larger that the input" when {
-      "given an integer" in {
+  "calculate" must {
+    "throw an error" when {
+      "right number is invalid" when {
+        "foo" in {
+          val core = new Core
+
+          val input = "2 + foo"
+          val result = core.calculate(input)
+
+          result mustBe Left("Error: Invalid input in right value. Expected a number but found 'foo'")
+        }
+
+        "bar" in {
+          val core = new Core
+
+          val input = "2 + bar"
+          val result = core.calculate(input)
+
+          result mustBe Left("Error: Invalid input in right value. Expected a number but found 'bar'")
+        }
+      }
+
+      "left number is invalid" when {
+        "foo" in {
+          val core = new Core
+
+          val input = "foo + 2"
+          val result = core.calculate(input)
+
+          result mustBe Left("Error: Invalid input in left value. Expected a number but found 'foo'")
+        }
+
+        "bar" in {
+          val core = new Core
+
+          val input = "bar + 2"
+          val result = core.calculate(input)
+
+          result mustBe Left("Error: Invalid input in left value. Expected a number but found 'bar'")
+        }
+      }
+
+      "both 'numbers' are invalid" when {
+        "foo and bar" in {
+          val core = new Core
+
+          val input = "foo + bar"
+          val result = core.calculate(input)
+
+          result mustBe Left("Error: Invalid input in both left and right values. Expected two numbers but found 'foo' and 'bar'")
+        }
+
+        "bar and foo" in {
+          val core = new Core
+
+          val input = "bar + foo"
+          val result = core.calculate(input)
+
+          result mustBe Left("Error: Invalid input in both left and right values. Expected two numbers but found 'bar' and 'foo'")
+        }
+      }
+
+      "operation is invalid" in {
         val core = new Core
 
-        val testValue = 1
-        val result = core.increment(testValue)
+        val input = "1 foo 2"
+        val result = core.calculate(input)
 
-        (result - testValue) mustBe 1
+        result mustBe Left("Error: Invalid input. Binary operator not supported")
+      }
+    }
+
+    "add 2 numbers" when {
+      "1 + 2" must {
+        "return 3" in {
+          val core = new Core
+
+          val input = "1 + 2"
+          val result = core.calculate(input)
+
+          result mustBe Right(3)
+        }
       }
     }
   }
