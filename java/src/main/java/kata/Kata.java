7b54e1ca111b0894a8ef1b7761a578ef2a8ebfcf
Dean <dean.kane@opencastsoftware.com>
2022-09-30 14:01:48 +0100

    Java progress on Kata interpolate

--- java/src/main/java/kata/Kata.java
+++ java/src/main/java/kata/Kata.java
@@ -1,7 +1,30 @@
 package kata;
 
+import java.util.HashMap;
+
 public class Kata {
     public int increment(int number) {
         return number + 1;
+    }
+
+    public String interpolate(String str, HashMap<String, String> map){
+
+
+        String[] strSplit = str.split(" " );
+        for(String s : strSplit){
+            if(s.startsWith("$") && s.endsWith("$")){
+                //Optional class perhaps use this.
+                str = str.replace(s, map.get(s.replace("$", "")));
+            }
+        }
+//        String strRemovedDollars = str.replace("$", "");
+//
+//
+//
+//        if(!map.isEmpty() && map.get(strRemovedDollars) != null){
+//            return map.get(strRemovedDollars);
+//        }
+
+        return str;
     }
 }
--- java/src/test/java/kata/KataTest.java
+++ java/src/test/java/kata/KataTest.java
@@ -4,10 +4,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 class KataTest {
     @Test
     void incrementAddsOne() {
-        var kata = new Kata();
+        Kata kata = new Kata();
         assertEquals(kata.increment(1), 2);
+    }
+
+    @Test
+    void returnEmptyString(){
+        Kata kata = new Kata();
+        HashMap<String, String> map = new HashMap<>();
+        String empty = "";
+        assertEquals("", kata.interpolate(empty, map));
+
+    }
+
+    @Test
+    void returnStringEmptyMap(){
+        Kata kata = new Kata();
+        HashMap<String, String> map = new HashMap<>();
+        String empty = "$temp$";
+        assertEquals("$temp$", kata.interpolate(empty, map));
+
+    }
+
+    @Test
+    void returnStringPopulatedMap(){
+        Kata kata = new Kata();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("temp", "temporary");
+        String empty = "$temp$";
+        assertEquals("temporary", kata.interpolate(empty, map));
+
+    }
+
+    @Test
+    void returnStringSentencePopulatedMap(){
+        Kata kata = new Kata();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("temp", "temporary");
+        map.put("name", "John Doe");
+        String str = "$temp$ here comes the name $name$";
+        assertEquals("temporary here comes the name John Doe", kata.interpolate(str, map));
+
     }
 }
